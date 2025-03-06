a818ad66f8578d0de6780069d3a46feab9c37256
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-03-07 00:03:36 +0530

    Integrated Gemini

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-
-dotenv.config();
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { marked } from "marked";
 
 const app = express();
+dotenv.config();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello!" });
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  // generationConfig: {
+  //   responseMimeType: "application/json",
+  // },
+});
+
+const message = "My son took my property illegally, what to do?";
+
+app.get("/", async (req, res) => {
+  const result = await model.generateContent(message);
+  res.status(200).send(marked(result.response.text()));
 });
 
 app.listen(process.env.BACKEND_PORT, () => {
